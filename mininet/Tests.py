2ed5b9d2076f968fbf3a0ef2a271c2e72eb0e083
Jonathan Dickerson <jonathan.dickerson@siu.edu>
2018-09-26 21:19:50 -0500

    started working on the tests

--- mininet/Tests.py
+++ mininet/Tests.py
@@ -8,50 +8,36 @@ import os
 import signal
 import time
 
-def iperfServerWrapper( host, port ):
-    serverCommand = 'iperf3 -s -i 1 -p %s -1 >> /tmp/%s/%sServer.log &' % ( port, host.name, host.name )
-    print serverCommand
-    host.cmd( serverCommand )
-
-def iperfClientWrapper( host, port, time, logPath ):
-    clientCommand = 'iperf3 -c %s -i 1 -p %s -t %s >> /tmp/%s/%sClient.log &' % ( host.IP(), port, time, logPath, logPath )
-    print clientCommand
-    host.cmd( clientCommand )
-
 class TestBaseClass( object ):
-     def __init__( self, hostOne, hostTwo, loggingDirectory, testDuration, network ):
-	 self.hostOne = hostOne
-	 self.hostTwo = hostTwo
-	 self.loggingDirectory = loggingDirectory
-	 self.testDuration = testDuration
-	 self.network = network
+	def __init__( self, loggingDirectory, testDuration, network ):
+		self.loggingDirectory = loggingDirectory
+		self.testDuration = testDuration
+		self.network = network
 
-    def runTest( self ):
-	pass
+	def runTest( self ):
+		pass
 
-class iperfTester( TestBaseClass ):
+class Efficiency( TestBaseClass ):
+	def __init__( self, loggingDirectory, testDuration, network ):
+		TestBaseClass.__init__( self, loggingDirectory, testDuration, network )
 
-    def __init__( self, hostOne, hostTwo, loggingDirectory, testDuration, network ):
-	TestBaseClass.__init__( self, hostOne, hostTwo, loggingDirectory, testDuration, network ):
+class LongFlowHandling( TestBaseClass ):
+	def __init__( self, loggingDirectory, testDuration, network ):
+		TestBaseClass.__init__( self, loggingDirectory, testDuration, network )
 
-    def runTest( self ):
-	jobs = []
-	port = 5001
-	for i in range( numberOfLinks ):
-	    p = multiprocessing.Process( target=iperfPair, args=( hostOne, hostTwo, testDuration, net, port ) )
-	    jobs.append( p )
-	    p.start()
-	    port += 2
+class RandomFlowHandling( TestBaseClass ):
+	def __init__( self, loggingDirectory, testDuration, network ):
+		TestBaseClass.__init__( self, loggingDirectory, testDuration, network )
 
-	for j in jobs:
-	    j.join()
-	    print 'Tester %s.exitcode = %s' % (j.name, j.exitcode)
+class ShortFlowHandling( TestBaseClass ):
+	def __init__( self, loggingDirectory, testDuration, network ):
+		TestBaseClass.__init__( self, loggingDirectory, testDuration, network )
 
-    def iperfPair( self, port ):
-	iperfServerWrapper( self.hostOne, int( port ) )
-	iperfServerWrapper( self.hostTwo, int( port ) + 1 )
+class WCBandwidthUtilization( TestBaseClass ):
+	def __init__( self, loggingDirectory, testDuration, network ):
+		TestBaseClass.__init__( self, loggingDirectory, testDuration, network )
 
-	time.sleep( 0.25 )
+class WCLogic( TestBaseClass ):
+	def __init__( self, loggingDirectory, testDuration, network ):
+		TestBaseClass.__init__( self, loggingDirectory, testDuration, network )
 
-	iperfClientWrapper( self.hostTwo, int( port ) + 1, self.testDuration, self.hostOne.name )
-	iperfClientWrapper( self.hostOne, int( port ), self.testDuration, self.hostTwo.name )
