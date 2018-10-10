27aae54dd204747c3aea8615a695ab9679aff738
Jonathan Dickerson <Jonathan.Dickerson@siu.edu>
2018-10-10 13:20:29 -0500

    more tests

--- mininet/Tests.py
+++ mininet/Tests.py
@@ -43,11 +43,18 @@ class WCLogic( TestBaseClass ):
 
     def runTest( self ):
 	numberOfHosts = len( self.network.hosts )
-	host = 1
-	while host <= numberOfHosts:
-	    print host
-	    host = host + 1
+	bandwidthValue = 100
 
+	for host in xrange( numberOfHosts ):
+	    for hostToRun in xrange( host ):
+		currentHost = self.network.getNodeByName( "h%s" % str( hostToRun + 1 ) )
+		print currentHost.IP(), currentHost.name
+		command = "./TestHandler -b %s -t 5 -l ""/home/jd/Desktop/test%s.log"" -r ""%s"" -z ""ClientServer"" -q" % currentHost.IP()
+#		print command
+#		commandReturns = currentHost.cmd( command )
+
+	    bandwidthValue = bandwidthValue + 100
+	    print bandwidthValue
 
 
 
